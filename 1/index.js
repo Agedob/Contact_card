66604fd7632f03acecca2d0e1bdc9362296ce637
MЯ.ƧᄂΣΣPY <agedob888@gmail.com>
2022-05-01 10:29:20 -0700

    switched to .on

--- 1/index.js
+++ 1/index.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-    
+    $(document).on("click", ':button', function(){
+        console.log('hello');
+        // console.log($('input[name="F_Name"').val());
+        // console.log($('.inputs').val())
+    })
 })
